d9ce5f753b4413f0003c76e1937aec9c211fc275
MIchele Lanning <mpdo2017@gmail.com>
2019-10-01 14:25:23 -0400

    updated CSS styling, JS functionality

---
+++ myrecipeapp/myrecipeapp/src/actions/recipeActions.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import {GET_ERRORS, GET_RECIPES, GET_RECIPE, DELETE_RECIPE} from "./types";
+
+export const createRecipe = (recipe, history) => async dispatch => {
+    try {
+        const res = await axios.post("http://localhost:8080/recipe", recipe);
+        history.push("/");
+        dispatch({
+            type   : GET_ERRORS,
+            payload: {}
+        });
+    } catch (err) {
+        dispatch({
+            type   : GET_ERRORS,
+            payload: err.response.data
+        });
+    }
+};
+
+export const getRecipes = () => async dispatch => {
+    const res = await axios.get("http://localhost:8080/recipe/all");
+    dispatch({
+        type   : GET_RECIPES,
+        payload: res.data
+    });
+};
+
+export const getRecipe = (id, history) => async dispatch => {
+    try {
+        const res = await axios.get(`http://localhost:8080/recipe/${id}`);
+        dispatch({
+            type   : GET_RECIPE,
+            payload: res.data
+        });
+    } catch (error) {
+        history.push("/");
+    }
+};
+
+export const deleteRecipe = (id, history) => async dispatch => {
+    await axios.delete(`http://localhost:8080/recipe/${id}`);
+    dispatch({
+        type   : DELETE_RECIPE,
+        payload: id
+    });
+};
---
+++ myrecipeapp/myrecipeapp/src/reducers/recipeReducer.js
@@ -0,0 +1,23 @@
+import {GET_RECIPES, GET_RECIPE} from "../actions/types";
+
+const initalState = {
+    recipes: [],
+    recipe : {}
+};
+
+export default function (state = initalState, action) {
+    switch (action.type) {
+        case GET_RECIPES:
+            return {
+                ...state,
+                recipes: action.payload
+            };
+        case GET_RECIPE:
+            return {
+                ...state,
+                recipe: action.payload
+            };
+        default:
+            return state;
+    }
+}
